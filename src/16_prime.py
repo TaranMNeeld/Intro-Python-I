82263116a060193312fffae13e53dc614f9184e1
Taran <thetaranneeld@gmail.com>
2020-02-11 13:54:20 -0700

    solved the prime number stretch

---
+++ src/16_prime.py
@@ -0,0 +1,24 @@
+def get_prime_numbers(max_num):
+    # make an array with all numbers between 2 and the input number
+    numbers = [int(i) for i in range(2, max_num)]
+
+    # loop through each number between 2 and the input number
+    for num in range(2, max_num):
+
+        # loop through every number from 2 to the (current number - 1)
+        for i in range(2, num):
+
+            # test if each number between 2 and the input number is divisible by any number between 2 and (itself - 1)
+            if num % i == 0:
+
+                # remove a number from the array if it is prime
+                numbers.remove(num)
+
+                # break so the loop can move on now that the number was removed
+                break
+
+    print(numbers)
+
+enter_num = input('Please enter a number:')
+
+get_prime_numbers(int(enter_num) + 1)
